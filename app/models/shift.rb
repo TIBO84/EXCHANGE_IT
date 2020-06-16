7277a2622af4d30eebe2ae9fbff639f7dc051835
Mael Harnois <mael.hrn@gmail.com>
2020-06-16 11:55:23 +0200

    update OK

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -4,5 +4,5 @@ class Shift < ApplicationRecord
   has_many :exchange_owners, class_name: "Exchange", foreign_key: :shift_owner_id
   has_many :exchange_answers, class_name: "Exchange", foreign_key: :shift_answer_id
   validates :date, :line_id, :hour_start, :hour_end, :working_hours, :user_id, presence: :true
-  validates :date, uniqueness: { scope: :user_id, message: “Un seul shift par jour !” }
+  validates :date, uniqueness: { scope: :user_id, message: "Un seul shift par jour !" }
 end
