9e49ff796373f284a87ce0f4fc50106dc63ff99d
Mael Harnois <mael.hrn@gmail.com>
2020-06-17 15:27:47 +0200

    update

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,22 +1,12 @@
 class DashboardsController < ApplicationController
   before_action :authenticate_user!
   before_action :clean_shifts, only: [:home]
-  before_action :set_current_user, only: [:home]
+  before_action :set_current_user, only: [:home, :my_shifts, :my_answers]
 
   def home
-  # Shift.joins().where(sql, user_id: current_user.id).order(:date)
-
-  # SELECT shifts.*
-  # from shifts
-  # INNER JOIN users ON users.id = shifts.user_id
-  # INNER JOIN lines ON lines.id = shifts.line_id
-  # LEFT JOIN exchanges ON (exchanges.shift_owner_id = shifts.id AND exchanges.accepted_owner IS TRUE) OR exchanges.shift_answer_id = shifts.id
-
-  # WHERE users.id != :user_id AND
-  #   users.unit_id = 7 AND
-  #   shifts.date > '2020-06-17' AND
-  #     exchanges.id IS NULL
-  # ORDER BY shifts.date
+    @shifts = Shift.joins(joins_sql)
+                   .where(where_sql, user_id: current_user.id, unit_id: current_user.unit_id, date: Date.today)
+                   .order(:date)
   end
 
   def my_shifts
@@ -36,5 +26,22 @@ class DashboardsController < ApplicationController
 
   def set_current_user
     @current_user = current_user
+  end
+
+  def where_sql
+    <<~SQL
+      users.id != :user_id AND
+      users.unit_id = :unit_id AND
+      shifts.date > :date AND
+      exchanges.id IS NULL
+    SQL
+  end
+
+  def joins_sql
+    <<~SQL
+      INNER JOIN users ON users.id = shifts.user_id
+      INNER JOIN lines ON lines.id = shifts.line_id
+      LEFT JOIN exchanges ON (exchanges.shift_owner_id = shifts.id AND exchanges.accepted_owner IS TRUE) OR exchanges.shift_answer_id = shifts.id
+    SQL
   end
 end
